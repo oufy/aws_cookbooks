80c526c8ef3d863303869185de89795e46c9496d
OufY <sebastien.longo@oufy.net>
2014-05-12 17:47:06 +0200

    OK

--- myinsead/recipes/upgrade_php5.rb
+++ myinsead/recipes/upgrade_php5.rb
@@ -2,6 +2,10 @@ execute 'exc_add_repo' do
   command 'add-apt-repository -y ppa:ondrej/php5-oldstable'
 end
 
+execute 'exc_update_cache' do
+  command 'apt-get update -y'
+end
+
 package 'php5' do
   action :upgrade
 end
