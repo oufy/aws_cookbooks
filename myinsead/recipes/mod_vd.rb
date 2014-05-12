124bc2101673811b6cc74709d77497f00e8d7c8d
OufY <sebastien.longo@oufy.net>
2014-05-12 18:49:49 +0200

    OK

---
+++ myinsead/recipes/mod_vd.rb
@@ -0,0 +1,16 @@
+directory 'my_insead_conf_dir' do
+  path '/etc/apache2/sites-available/myinsead.conf.d'
+  action :create
+  mode '755'
+  owner 'root'
+  group 'root'
+end
+
+file 'local_directory_config_file' do
+  path '/etc/apache2/sites-available/myinsead.conf.d/local_directories.conf'
+  action :create_if_missing
+  source 'local_directories.conf.erb'
+  mode '644'
+  owner 'root'
+  group 'root'
+end
